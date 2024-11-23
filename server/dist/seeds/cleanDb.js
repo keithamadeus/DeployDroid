1cd3786f40fea96665fb3922af8a60c2d86e6dbb
Keith <keith.amadeus.williams@gmail.com>
2024-11-22 17:54:35 -0700

    npm i, build, seed, updated cleanDB.ts

---
+++ server/dist/seeds/cleanDb.js
@@ -0,0 +1,18 @@
+import models from '../models/index.js';
+import db from '../config/connection.js';
+export default async (modelName, collectionName) => {
+    try {
+        if (!models[modelName] || !models[modelName].db) {
+            throw new Error(`Model or database not found for modelName: ${modelName}`);
+        }
+        let modelExists = await models[modelName]?.db?.db?.listCollections({
+            name: collectionName
+        }).toArray();
+        if (modelExists && modelExists.length) {
+            await db.dropCollection(collectionName);
+        }
+    }
+    catch (err) {
+        throw err;
+    }
+};
